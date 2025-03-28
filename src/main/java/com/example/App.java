1a19cd258d496ef6899017772c324b27eee98829
Lakshmi1799 <lakshmisravanthi017@gmail.com>
2025-03-28 22:20:39 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App {
     @GetMapping("/")
     public String home(Model model) {
         // Pass the header text to the view
-        model.addAttribute("header", "This is Sravanthi's website");
+        model.addAttribute("header", "CCIT-Banglore");
         return "index"; // Return the index.html template
     }
 }
